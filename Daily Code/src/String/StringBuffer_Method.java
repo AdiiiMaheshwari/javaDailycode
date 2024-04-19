b4f6be3715cf6bad2ec0e7dff3f3657fbe70bc11
AdiiiMaheshwari <121353460+AdiiiMaheshwari@users.noreply.github.com>
2024-04-19 14:59:55 +0530

    Add files via upload

---
+++ Daily Code/src/String/StringBuffer_Method.java
@@ -0,0 +1,30 @@
+package String;
+
+public class StringBuffer_Method {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Hello");
+
+        //APPEND
+        //sb.append("java");
+        //System.out.println("Append String is : "+sb);
+
+        //INSERT
+//        sb.insert(1,"java");
+//        System.out.println("Insert string is : "+sb);
+
+        //REPLACE
+        //sb.replace(1,3,"Adii");
+        //System.out.println("Replace String is : "+sb);
+
+        //DELETE
+        //sb.delete(1,3);
+        //System.out.println("Delete String is : "+sb);
+
+        //REVERSE
+        //sb.reverse();
+        //System.out.println("Reverse String is : "+sb);
+
+        //
+
+    }
+}
---
+++ Daily Code/src/String/StringBuilder_Method.java
@@ -0,0 +1,19 @@
+package String;
+
+public class StringBuilder_Method {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+
+        //APPEND
+        //sb.append("java");
+        //System.out.println("Append String is : "+sb);
+
+        //INSERT
+        //sb.insert(1,"java");
+        //System.out.println("Insert String is : "+sb);
+
+
+
+
+    }
+}
